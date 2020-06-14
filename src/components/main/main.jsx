40c78102115a3cb943f532456b6a1421c233bf62
Artemiy <artemiy.dmitrochenko@gmail.com>
2020-06-15 04:41:40 +0500

    Переносит событие onClick на ссылку и обновляет снепшоты

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -11,14 +11,18 @@ const generateMovieCard = (title, index, onTitleClick) => {
   return (
     <article
       key={key}
-      onClick={onTitleClick}
       className="small-movie-card catalog__movies-card"
     >
       <div className="small-movie-card__image">
         <img src={poster} alt={title} width="280" height="175"/>
       </div>
       <h3 className="small-movie-card__title">
-        <a className="small-movie-card__link" href="movie-page.html">{title}</a>
+        <a
+          className="small-movie-card__link"
+          href="movie-page.html"
+          onClick={onTitleClick}
+        >{title}
+        </a>
       </h3>
     </article>
   );
