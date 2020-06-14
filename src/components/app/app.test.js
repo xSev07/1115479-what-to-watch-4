d741c89a33a7cfe4e7a9e796a631a838a4dfa6cc
Artemiy <artemiy.dmitrochenko@gmail.com>
2020-06-14 20:02:04 +0500

    Добавляет снепшот тесты к существующим компонентам

---
+++ src/components/app/app.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import App from "./app";
+
+const title = `The Grand Budapest Hotel`;
+const genre = `Drama`;
+const year = 2014;
+const movies = [
+  `Bohemian Rhapsody`,
+  `Macbeth`,
+  `Aviator`
+];
+
+it(`Should App render correctly`, () => {
+  const tree = renderer
+    .create(<App
+      promoTitle={title}
+      promoGenre={genre}
+      promoYear={year}
+      allMovies={movies}
+    />);
+
+  expect(tree).toMatchSnapshot();
+});
---
+++ src/components/main/main.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Main from "./main";
+
+const title = `The Grand Budapest Hotel`;
+const genre = `Drama`;
+const year = 2014;
+const movies = [
+  `Bohemian Rhapsody`,
+  `Macbeth`,
+  `Aviator`
+];
+
+it(`Should Main render correctly`, () => {
+  const tree = renderer
+    .create(<Main
+      title={title}
+      genre={genre}
+      year={year}
+      movies={movies}
+    />);
+
+  expect(tree).toMatchSnapshot();
+});
