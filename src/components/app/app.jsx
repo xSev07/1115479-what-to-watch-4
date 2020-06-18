6c13ca2c2f8afbd92c62eac241b9bcf48e49fd4b
Artemiy <artemiy.dmitrochenko@gmail.com>
2020-06-19 03:26:38 +0500

    Подключает react-router в app, и переделывает app в класс

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,46 @@
 import React from "react";
 import Main from "../main/main.jsx";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+import {PureComponent} from "react/cjs/react.production.min";
+import MoviePage from "../movie-page/movie-page.jsx";
 
-const App = (props) => {
-  const {promoMovie, allMovies, onMovieTitleClick} = props;
-  return (
-    <Main
-      promo={promoMovie}
-      movies={allMovies}
-      onTitleClick={onMovieTitleClick}
-    />
-  );
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderMainScreen()}
+          </Route>
+          <Route exact path="/dev-movie">
+            {this._renderMovieScreen()}
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+
+  _renderMainScreen() {
+    const {promoMovie, allMovies, onMovieTitleClick} = this.props;
+    return (
+      <Main
+        promo={promoMovie}
+        movies={allMovies}
+        onTitleClick={onMovieTitleClick}
+      />
+    );
+  }
+
+  _renderMovieScreen() {
+    return (
+      <MoviePage/>
+    );
+  }
 };
 
 App.propTypes = {
