0693cdea6f2051e26d54ebb95a3f8d2c11a235fa
Artemiy <artemiy.dmitrochenko@gmail.com>
2020-06-20 11:14:21 +0500

    Дополняет моки

--- src/tests-data/tests-data.js
+++ src/tests-data/tests-data.js
@@ -1,45 +1,64 @@
 export const movies = [
   {
-    id: `Bohemian-2000`,
-    actors: [],
-    description: [],
-    duration: 0,
+    id: `Bohemian-2018`,
+    actors: [
+      `Rami Malek`,
+      `Lucy Boynton`,
+      `Gwilym Lee`
+    ],
+    description: [
+      `The story of the legendary British rock band Queen and lead singer Freddie Mercury, leading up to their famous performance at Музыкальный фестиваль Live Aid (1985)`
+    ],
+    duration: 134,
     genre: ``,
     inList: false,
-    producer: ``,
+    producer: `Bryan Singer`,
     rating: 8.0,
     reviews: [],
     title: `Bohemian Rhapsody`,
     votes: 220,
-    year: 2000,
+    year: 2018,
   },
   {
-    id: `Macbeth-2000`,
-    actors: [],
-    description: [],
-    duration: 0,
+    id: `Macbeth-2015`,
+    actors: [
+      `Michael Fassbender`,
+      `Marion Cotillard`,
+      `Paddy Considine`
+    ],
+    description: [
+      `From the Academy Award winning producers of The King’s Speech and acclaimed director Justin Kurzel, comes a visceral and visually breath-taking retelling of the classic tale about an ambitious Scottish lord who seizes the throne with the help of his wife. Starring Academy Award nominee Michael Fassbender and Academy Award winner Marion Cotillard, Macbeth is both visually epic and a breathtaking experience.`
+    ],
+    duration: 113,
     genre: ``,
     inList: false,
-    producer: ``,
-    rating: 8.0,
+    producer: `Justin Kurzel`,
+    rating: 6.6,
     reviews: [],
     title: `Macbeth`,
     votes: 220,
-    year: 2000,
+    year: 2015,
   },
   {
-    id: `Aviator-2000`,
-    actors: [],
-    description: [],
-    duration: 0,
+    id: `Aviator-2005`,
+    actors: [
+      `Leonardo DiCaprio`,
+      `Cate Blanchett`,
+      `Kate Beckinsale`
+    ],
+    description: [
+      `The Aviator is a 2004 American epic biographical drama film directed by Martin Scorsese and written by John Logan. It stars Leonardo DiCaprio as Howard Hughes, Cate Blanchett as Katharine Hepburn, and Kate Beckinsale as Ava Gardner. The supporting cast features Ian Holm, John C. Reilly, Alec Baldwin, Jude Law as Errol Flynn, Gwen Stefani as Jean Harlow, Kelli Garner as Faith Domergue, Matt Ross, Willem Dafoe, Alan Alda, and Edward Herrmann.`,
+      `Based on the 1993 non-fiction book Howard Hughes: The Secret Life by Charles Higham, the film depicts the life of Howard Hughes, an aviation pioneer and director of Hell's Angels. The film portrays his life from 1927–1947 during which time Hughes became a successful film producer and an aviation magnate while simultaneously growing more unstable due to severe obsessive–compulsive disorder (OCD).`
+    ],
+    duration: 170,
     genre: ``,
     inList: false,
-    producer: ``,
-    rating: 8.0,
+    producer: `Martin Scorsese`,
+    rating: 7.5,
     reviews: [],
     title: `Aviator`,
     votes: 220,
-    year: 2000,
+    year: 2005,
   },
 ];
 
