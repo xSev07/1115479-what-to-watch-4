de7c32bffcf31ce9ee3ad0ee638c278ca06b89b0
Artemiy <artemiy.dmitrochenko@gmail.com>
2020-07-08 21:42:29 +0500

    Добавляет api, подключает api к приложению

--- src/index.js
+++ src/index.js
@@ -2,13 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 import movies from "./mocks/movies";
-import {createStore} from "redux";
-import {reducer} from "./reducer";
+import {applyMiddleware, createStore} from "redux";
+import {ActionCreator, AuthorizationStatus, reducer} from "./reducer";
 import {Provider} from "react-redux";
+import {createAPI} from "./api";
+import {composeWithDevTools} from "redux-devtools-extension";
+import thunk from "redux-thunk";
+
+const onUnauthorized = () => {
+  store.dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.NO_AUTH));
+};
+
+const api = createAPI(onUnauthorized);
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    composeWithDevTools(
+        applyMiddleware(thunk.withExtraArgument(api))
+    )
 );
 
 const promoMovie = {
--- src/reducer.js
+++ src/reducer.js
@@ -1,14 +1,21 @@
 import {extendObject} from "./const";
 import movies from "./mocks/movies";
 
+const AuthorizationStatus = {
+  AUTH: `AUTH`,
+  NO_AUTH: `NO_AUTH`,
+};
+
 const initialState = {
   genre: `all genres`,
   movies,
+  authorizationStatus: AuthorizationStatus.NO_AUTH,
 };
 
 const ActionType = {
   SET_FILTER_GENRE: `CHANGE_FILTER_GENRE`,
   GET_MOVIES_WITH_GENRE: `GET_MOVIES_WITH_GENRE`,
+  REQUIRED_AUTHORIZATION: `REQUIRED_AUTHORIZATION`,
 };
 
 const ActionCreator = {
@@ -16,15 +23,21 @@ const ActionCreator = {
     type: ActionType.SET_FILTER_GENRE,
     payload: genre || `all`,
   }),
+  requireAuthorization: (status) => ({
+    type: ActionType.REQUIRED_AUTHORIZATION,
+    payload: status,
+  }),
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ActionType.SET_FILTER_GENRE:
       return extendObject(state, {genre: action.payload});
+    case ActionType.REQUIRED_AUTHORIZATION:
+      return extendObject(state, {authorizationStatus: action.payload});
   }
 
   return state;
 };
 
-export {reducer, ActionType, ActionCreator};
+export {reducer, ActionType, ActionCreator, AuthorizationStatus};
