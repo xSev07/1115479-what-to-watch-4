501165c3c11953bad451308aed08a52c9f8fb5fc
Artemiy <artemiy.dmitrochenko@gmail.com>
2020-07-12 22:03:57 +0500

    Добавляет загрузку комментариев с сервера

--- src/adapters/comments.js
+++ src/adapters/comments.js
@@ -1,7 +1,13 @@
-// TODO: Реализовать парсинг комментариев
-
 const parseComment = (comment) => {
-  return comment;
+  const userData = comment[`user`];
+  return {
+    id: comment[`id`],
+    userId: userData[`id`],
+    userName: userData[`name`],
+    rating: comment[`rating`],
+    text: comment[`comment`],
+    date: comment[`date`],
+  };
 };
 
 const parseComments = (data) => {
