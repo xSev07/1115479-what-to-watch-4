d0a30cba326d3b36db6e552d1749c75c8fb695f7
Artemiy <artemiy.dmitrochenko@gmail.com>
2020-07-15 00:54:04 +0500

    Обновляет снепшот тест у MovieDescription

--- src/components/movie-description/movie-description.test.js
+++ src/components/movie-description/movie-description.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import MovieDescription from "./movie-description";
+import {MovieDescription} from "./movie-description";
 import {comments, movies} from "../../tests-data/tests-data";
 
 it(`Should MovieDescription render correctly`, () => {
@@ -9,7 +9,9 @@ it(`Should MovieDescription render correctly`, () => {
         <MovieDescription
           movie={movies[0]}
           comments={comments}
-          loadComments={() => {}}
+          elements={[`Overview`, `Details`, `Reviews`]}
+          activeElement={`Overview`}
+          onElementClick={() => {}}
         />
     ).toJSON();
 
