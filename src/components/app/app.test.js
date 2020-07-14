cb6fb0af136d99b5e6f3d1088cc6f29a89365f22
Artemiy <artemiy.dmitrochenko@gmail.com>
2020-07-14 16:52:09 +0500

    Восстанавливает снепшот тест App

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,27 +1,30 @@
-// import React from "react";
-// import renderer from "react-test-renderer";
-// import App from "./app";
-// import {storeData} from "../../tests-data/tests-data";
-// import {Provider} from "react-redux";
-// import configureStore from "redux-mock-store";
-//
-// const mockStore = configureStore([]);
+import React from "react";
+import renderer from "react-test-renderer";
+import {App} from "./app";
+import {movies, promoMovie, storeData} from "../../tests-data/tests-data";
+import {Provider} from "react-redux";
+import configureStore from "redux-mock-store";
+
+const mockStore = configureStore([]);
 
 it(`Should App render correctly`, () => {
-  // TODO: Восстановить работоспособность теста
-  return undefined;
-  // const store = mockStore(storeData);
-  //
-  // const tree = renderer
-  //   .create(
-  //       <Provider store={store} >
-  //         <App/>
-  //       </Provider>, {
-  //         createNodeMock: () => {
-  //           return {};
-  //         }
-  //       }
-  //   ).toJSON();
-  //
-  // expect(tree).toMatchSnapshot();
+  const store = mockStore(storeData);
+
+  const tree = renderer
+    .create(
+        <Provider store={store} >
+          <App
+            loadPromo={() => {}}
+            loadMovies={() => {}}
+            allMovies={movies}
+            promoMovie={promoMovie}
+          />
+        </Provider>, {
+          createNodeMock: () => {
+            return {};
+          }
+        }
+    ).toJSON();
+
+  expect(tree).toMatchSnapshot();
 });
