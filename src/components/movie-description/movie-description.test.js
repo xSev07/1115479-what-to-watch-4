47a21f551b7394313492be088247c49c581b5f78
Artemiy <artemiy.dmitrochenko@gmail.com>
2020-07-14 22:44:16 +0500

    Обновляет снепшоты для новых компонентов

--- src/components/movie-description/movie-description.test.js
+++ src/components/movie-description/movie-description.test.js
@@ -1,21 +1,14 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import {MovieDescription} from "./movie-description";
-import {movies} from "../../tests-data/tests-data";
+import MovieDescription from "./movie-description";
+import {comments, movies} from "../../tests-data/tests-data";
 
 it(`Should MovieDescription render correctly`, () => {
   const tree = renderer
     .create(
         <MovieDescription
-          {...movies[0]}
-          comments={[{
-            commentId: `1`,
-            userId: `10`,
-            author: `Test`,
-            rating: 5.2,
-            text: `Test string`,
-            date: new Date(`1995-12-17T03:24:00`),
-          }]}
+          movie={movies[0]}
+          comments={comments}
           loadComments={() => {}}
         />
     ).toJSON();
