f8f6db2b4f6ad6e5a7e9408b80aa61c4a6826a8a
Artemiy <artemiy.dmitrochenko@gmail.com>
2020-07-22 04:05:25 +0500

    Добавляет e2e тест для SignIn

---
+++ src/components/sign-in/sign-in.e2e.test.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Enzyme, {mount} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import {Provider} from "react-redux";
+import {SignIn} from "./sign-in";
+import configureStore from "redux-mock-store";
+import {storeData} from "../../tests-data/tests-data";
+import {StaticRouter} from "react-router-dom";
+
+Enzyme.configure({
+  adapter: new Adapter(),
+});
+
+const mockStore = configureStore([]);
+
+const store = mockStore(storeData);
+
+describe(`SignIn component`, () => {
+  it(`should check form submiting`, () => {
+    const onFormSubmit = jest.fn();
+
+    const signIn = mount(
+        <Provider store={store}>
+          <StaticRouter>
+            <SignIn
+              authError={false}
+              incorrectEmail={false}
+              incorrectPassword={false}
+              handleFormSubmit={onFormSubmit}
+            />
+          </StaticRouter>
+        </Provider>, {
+          createNodeMock: () => {}
+        }
+    );
+
+    const emailInput = signIn.find(`input#user-email.sign-in__input`);
+    emailInput.instance().value = `example@gmail.com`;
+
+    const passwordInput = signIn.find(`input#user-password.sign-in__input`);
+    passwordInput.instance().value = `qwerty`;
+
+    const submitButton = signIn.find(`button.sign-in__btn`);
+    submitButton.simulate(`click`);
+
+    expect(onFormSubmit).toHaveBeenCalledTimes(1);
+    expect(onFormSubmit).toHaveBeenNthCalledWith(1,
+        {
+          loginValue: `example@gmail.com`,
+          passwordValue: `qwerty`,
+        });
+  });
+});
