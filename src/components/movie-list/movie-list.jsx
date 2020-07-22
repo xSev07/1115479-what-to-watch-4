fe78b337b164100d40a5eba2d86a489edc466dd5
Artemiy <artemiy.dmitrochenko@gmail.com>
2020-07-22 19:56:14 +0500

    Переносит static router в тесты

--- src/components/movie-list/movie-list.jsx
+++ src/components/movie-list/movie-list.jsx
@@ -1,30 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
 import MovieCard from "../movie-card/movie-card.jsx";
-import {StaticRouter} from "react-router-dom";
 
 const MovieList = (props) => {
   const {movies} = props;
 
   return (
     <div className="catalog__movies-list">
-      <StaticRouter>
-        {
-          movies.map((movie) => {
-            return (
-
-              <MovieCard
-                key={movie.id}
-                id={movie.id}
-                title={movie.title}
-                videoPreview={movie.videoPreview}
-                poster={movie.preview}
-                isMuted={true}
-              />
-            );
-          })
-        }
-      </StaticRouter>
+      {
+        movies.map((movie) => {
+          return (
+            <MovieCard
+              key={movie.id}
+              id={movie.id}
+              title={movie.title}
+              videoPreview={movie.videoPreview}
+              poster={movie.preview}
+              isMuted={true}
+            />
+          );
+        })
+      }
     </div>
   );
 };
