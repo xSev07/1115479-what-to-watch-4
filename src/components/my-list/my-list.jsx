995109c5f02a71a2a7e92ec76aa52857bec7a7be
Artemiy <artemiy.dmitrochenko@gmail.com>
2020-07-24 05:25:45 +0500

    8-2: Добавляет логику загрузки избранных фильмов и оживляет SignIn

--- src/components/my-list/my-list.jsx
+++ src/components/my-list/my-list.jsx
@@ -1,109 +1,94 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Header from "../header/header.jsx";
 import Footer from "../footer/footer.jsx";
+import MovieList from "../movie-list/movie-list.jsx";
+import {connect} from "react-redux";
+import {
+  getFavoriteMovies,
+  getFavoriteMoviesLoadingError,
+  getFavoriteMoviesLoadingStatus
+} from "../../reducer/data/selectors";
+import {PureComponent} from "react/cjs/react.production.min";
+import {Operation as DataOperation} from "../../reducer/data/data";
+import {LoadError, Spinner} from "../svg/svg.jsx";
 
-const MyList = () => {
-  return (
-    <div className="user-page">
-      <Header
-        className={`user-page__head`}
-      >
-        <h1 className="page-title user-page__title">My list</h1>
-      </Header>
+class MyList extends PureComponent {
+  constructor(props) {
+    super(props);
 
-      <section className="catalog">
-        <h2 className="catalog__title visually-hidden">Catalog</h2>
+    this._getFavoriteMoviesTemplate = this._getFavoriteMoviesTemplate.bind(this);
+  }
 
-        <div className="catalog__movies-list">
-          <article className="small-movie-card catalog__movies-card">
-            <div className="small-movie-card__image">
-              <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt="Fantastic Beasts: The Crimes of Grindelwald" width="280" height="175"/>
-            </div>
-            <h3 className="small-movie-card__title">
-              <a className="small-movie-card__link" href="movie-page.html">Fantastic Beasts: The Crimes of
-                Grindelwald</a>
-            </h3>
-          </article>
+  render() {
+    return (
+      <div className="user-page">
+        <Header
+          className={`user-page__head`}
+        >
+          <h1 className="page-title user-page__title">My list</h1>
+        </Header>
 
-          <article className="small-movie-card catalog__movies-card">
-            <div className="small-movie-card__image">
-              <img src="img/bohemian-rhapsody.jpg" alt="Bohemian Rhapsody" width="280" height="175"/>
-            </div>
-            <h3 className="small-movie-card__title">
-              <a className="small-movie-card__link" href="movie-page.html">Bohemian Rhapsody</a>
-            </h3>
-          </article>
+        <section className="catalog">
+          <h2 className="catalog__title visually-hidden">Catalog</h2>
 
-          <article className="small-movie-card catalog__movies-card">
-            <div className="small-movie-card__image">
-              <img src="img/macbeth.jpg" alt="Macbeth" width="280" height="175"/>
-            </div>
-            <h3 className="small-movie-card__title">
-              <a className="small-movie-card__link" href="movie-page.html">Macbeth</a>
-            </h3>
-          </article>
+          {this._getFavoriteMoviesTemplate()}
+        </section>
 
-          <article className="small-movie-card catalog__movies-card">
-            <div className="small-movie-card__image">
-              <img src="img/aviator.jpg" alt="Aviator" width="280" height="175"/>
-            </div>
-            <h3 className="small-movie-card__title">
-              <a className="small-movie-card__link" href="movie-page.html">Aviator</a>
-            </h3>
-          </article>
+        <Footer/>
+      </div>
+    );
+  }
 
+  _getFavoriteMoviesTemplate() {
+    const {movies, loadingError, isLoading} = this.props;
 
-          <article className="small-movie-card catalog__movies-card">
-            <div className="small-movie-card__image">
-              <img src="img/we-need-to-talk-about-kevin.jpg" alt="We need to talk about Kevin" width="280" height="175"/>
-            </div>
-            <h3 className="small-movie-card__title">
-              <a className="small-movie-card__link" href="movie-page.html">We need to talk about Kevin</a>
-            </h3>
-          </article>
-
-          <article className="small-movie-card catalog__movies-card">
-            <div className="small-movie-card__image">
-              <img src="img/what-we-do-in-the-shadows.jpg" alt="What We Do in the Shadows" width="280" height="175"/>
-            </div>
-            <h3 className="small-movie-card__title">
-              <a className="small-movie-card__link" href="movie-page.html">What We Do in the Shadows</a>
-            </h3>
-          </article>
-
-          <article className="small-movie-card catalog__movies-card">
-            <div className="small-movie-card__image">
-              <img src="img/revenant.jpg" alt="Revenant" width="280" height="175"/>
-            </div>
-            <h3 className="small-movie-card__title">
-              <a className="small-movie-card__link" href="movie-page.html">Revenant</a>
-            </h3>
-          </article>
-
-          <article className="small-movie-card catalog__movies-card">
-            <div className="small-movie-card__image">
-              <img src="img/johnny-english.jpg" alt="Johnny English" width="280" height="175"/>
-            </div>
-            <h3 className="small-movie-card__title">
-              <a className="small-movie-card__link" href="movie-page.html">Johnny English</a>
-            </h3>
-          </article>
-
-
-          <article className="small-movie-card catalog__movies-card">
-            <div className="small-movie-card__image">
-              <img src="img/shutter-island.jpg" alt="Shutter Island" width="280" height="175"/>
-            </div>
-            <h3 className="small-movie-card__title">
-              <a className="small-movie-card__link" href="movie-page.html">Shutter Island</a>
-            </h3>
-          </article>
+    if (loadingError) {
+      return (
+        <div style={{marginTop: `50px`, display: `flex`, flexDirection: `column`, justifyContent: `center`, alignItems: `center`}}>
+          <h2 style={{color: `#d9cd8d`}}>Извините, у нашего сервера лапки. Попробуйте позднее</h2>
+          <br/>
+          <LoadError/>
         </div>
-      </section>
+      );
+    }
+    if (isLoading) {
+      return (
+        <div style={{marginTop: `50px`, display: `flex`, justifyContent: `center`, alignItems: `center`}}>
+          <Spinner/>
+        </div>
+      );
+    }
+    return <MovieList movies={movies}/>;
+  }
 
-      <Footer/>
-    </div>
-  );
+  componentDidMount() {
+    this.props.loadFavoriteMovies();
+  }
+}
+
+const mapStateToProps = (state) => ({
+  movies: getFavoriteMovies(state),
+  loadingError: getFavoriteMoviesLoadingError(state),
+  isLoading: getFavoriteMoviesLoadingStatus(state),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  loadFavoriteMovies() {
+    dispatch(DataOperation.loadFavoriteMovies());
+  }
+});
+
+MyList.propTypes = {
+  movies: PropTypes.arrayOf(
+      PropTypes.shape({
+        title: PropTypes.string.isRequired,
+      })
+  ).isRequired,
+  loadingError: PropTypes.bool.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  loadFavoriteMovies: PropTypes.func.isRequired,
 };
 
-export default MyList;
+export {MyList};
+export default connect(mapStateToProps, mapDispatchToProps)(MyList);
--- src/components/my-list/my-list.test.js
+++ src/components/my-list/my-list.test.js
@@ -1,23 +1,78 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import MyList from "./my-list";
+import {MyList} from "./my-list";
 import configureStore from "redux-mock-store";
-import {storeData} from "../../tests-data/tests-data";
+import {movies, storeData} from "../../tests-data/tests-data";
 import {Provider} from "react-redux";
 import {StaticRouter} from "react-router-dom";
 
 const mockStore = configureStore([]);
 const store = mockStore(storeData);
 
-it(`Should MyList render correctly`, () => {
-  const tree = renderer
+describe(`Should MyList render correctly`, () => {
+  it(`Should render without errors`, () => {
+    const tree = renderer
     .create(
         <Provider store={store}>
           <StaticRouter>
-            <MyList/>
+            <MyList
+              movies={movies}
+              loadingError={false}
+              isLoading={false}
+              loadFavoriteMovies={() => {}}
+            />
           </StaticRouter>
-        </Provider>
+        </Provider>, {
+          createNodeMock: () => {
+            return {};
+          }
+        }
     ).toJSON();
 
-  expect(tree).toMatchSnapshot();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it(`Should render with load error`, () => {
+    const tree = renderer
+      .create(
+          <Provider store={store}>
+            <StaticRouter>
+              <MyList
+                movies={movies}
+                loadingError={true}
+                isLoading={false}
+                loadFavoriteMovies={() => {}}
+              />
+            </StaticRouter>
+          </Provider>, {
+            createNodeMock: () => {
+              return {};
+            }
+          }
+      ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it(`Should render with waiting download`, () => {
+    const tree = renderer
+      .create(
+          <Provider store={store}>
+            <StaticRouter>
+              <MyList
+                movies={movies}
+                loadingError={false}
+                isLoading={true}
+                loadFavoriteMovies={() => {}}
+              />
+            </StaticRouter>
+          </Provider>, {
+            createNodeMock: () => {
+              return {};
+            }
+          }
+      ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
 });
