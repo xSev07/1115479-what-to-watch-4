7e6316577a3ecf554f3c931a5e271d67bec842dd
Artemiy <artemiy.dmitrochenko@gmail.com>
2020-08-02 04:55:28 +0500

    Дополняет е2е тест add review form(WIP)

--- src/components/add-review-form/add-review-form.e2e.test.js
+++ src/components/add-review-form/add-review-form.e2e.test.js
@@ -1,7 +1,7 @@
-import React from "react";
-import Enzyme, {shallow} from "enzyme";
+// import React from "react";
+import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import AddReviewForm from "./add-review-form";
+// import AddReviewForm from "./add-review-form";
 
 Enzyme.configure({
   adapter: new Adapter(),
@@ -13,8 +13,17 @@ describe(`Should check AddReviewForm work correctly`, () => {
       return true;
     });
     // const onSubmit = jest.fn();
-    // const onChange = jest.fn();
-    //
+    // const onChange = jest.fn((evt) => {
+    //   if (evt.target.name === `text`) {
+    //     formData.text = evt.target.value;
+    //   } else if (evt.target.name === `rating`) {
+    //     formData.rating = evt.target.value;
+    //   }
+    // });
+    // const formData = {
+    //   rating: `0`,
+    //   text: ``,
+    // };
     // const addReviewForm = shallow(
     //     <AddReviewForm
     //       isSubmitDisabled={false}
@@ -30,14 +39,20 @@ describe(`Should check AddReviewForm work correctly`, () => {
     // const t2 = t1.current;
 
     // const textField = addReviewForm.find(`.add-review__textarea`);
-    // textField.simulate(`change`, {target: {value: `sometext`}});
-    //
-    //
+    // textField.simulate(`change`, {target: {name: `text`, value: `sometext`}});
+
+    // const textField = addReviewForm.find(`.add-review__textarea`);
+    // textField.simulate(`change`, {target: {name: `text`, value: `sometext`}});
+
     // const submitButton = addReviewForm.find(`.add-review__btn`);
     // submitButton.simulate(`click`, {
     //   preventDefault: () => {}
     // });
-    //
-    // expect(onSubmit).toHaveBeenCalledTimes(1);
+
+    // expect(onChange).toHaveBeenCalledTimes(1);
+    // expect(onChange).toEqual({
+    //   rating: `0`,
+    //   text: `sometext`,
+    // });
   });
 });
